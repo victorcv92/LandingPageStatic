ac711ffa406843b66fb42cc19739dd1d04cc4bce
victorcv92 <johan.calderonva@gmail.com>
2024-04-20 01:54:43 -0500

    actualizaciones

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,11 +19,15 @@ module.exports = {
         'lps-color-light': 'rgb(0,105,255)',
         'lps-color-medium': 'rgb(20,86,255)',
         'lps-color-mediumdark': '#1d274c',
+        'lps-color-ligther': '#f9fafe',
 
       },
       boxShadow:{
         'input':'0 4px 1px 0 rgb(0, 0, 0, 0.1)',
         'inputfocus': '0 2px 1px 0 rgb(0, 0, 0, 0.1)',
+      },
+      width:{
+        '88':'22rem',
       },
     },
   },
